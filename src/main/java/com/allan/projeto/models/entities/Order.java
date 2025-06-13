692e35bbec1d662796f8702683f6ddfeb250770b
Allan Almeida <allan.almeida.dev@gmail.com>
2025-06-13 17:17:04 -0300

    criado o relacionamento de user com order

---
+++ src/main/java/com/allan/projeto/models/entities/Order.java
@@ -0,0 +1,66 @@
+package com.allan.projeto.models.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client_id;
+
+    public Order() {}
+
+    public Order(Long id, Instant moment) {
+        this.id = id;
+        this.moment = moment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public User getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(User client_id) {
+        this.client_id = client_id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
